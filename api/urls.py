f54d58aaf78720736dc28f8558e0e07f4a34deb5
Jhon Bohorquez <jhon.bohorquez@ariadnacg.com>
2022-02-18 12:39:37 -0500

    load 6

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import RequesForms
 
 urlpatterns = [
+    path('', RequesForms.hola),
     path('form_1/', RequesForms.as_view(), name='formulario 1'),
     # path('data/', GetData.as_view(), name='get data')
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Model_Forms
 from django.views import View
 import requests
 import json 
 
 class RequesForms(View):
+    def hola(request):
+        return HttpResponse('<h1>HOLA</h1>')
+
     def get(self, request):
         Clist= Model_Forms.objects.all()
         return JsonResponse(list(Clist.values()), safe=False)
